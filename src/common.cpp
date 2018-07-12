35fe9ff010cdfd0871c39f9287970e3634471718
root <root@rac1.prod.vmware.haf>
2018-07-12 23:06:50 +0200

    src/common.cpp avoid debug prints

--- src/common.cpp
+++ src/common.cpp
@@ -284,15 +284,10 @@ std::string slurp(const char *filename)
 
 std::vector<unsigned char> hex2bytes(const std::string& hex) {
 	std::vector<unsigned char> bytes;
-
-	std::cout << "hex2bytes: ";
 	for (unsigned int i = 0; i < hex.length(); i += 2) {
 		std::string byteString = hex.substr(i, 2);
 		char byte = (unsigned char) strtol(byteString.c_str(), NULL, 16);
 		bytes.push_back(byte);
-		std::cout << std::hex << (unsigned int)byte << ":";
 	}
-	std::cout << std::endl;
-	std::cout << std::dec;
 	return bytes;
 }
